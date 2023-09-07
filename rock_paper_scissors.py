69efec56c2df8e21497e94c263d910c0cb25be07
Cristian <cnrosario@cps.edu>
2023-09-07 11:31:07 -0500

    did some a1.py answers dont know if im close

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,14 @@
-# We will write a rock paper scissors game in class - Complete it in this file
+
+import random
+player_choice ="rock"
+computer_choice= "paer"
+
+def get_choices():
+    player_choice = "rock"
+    computer_choice= "paper"
+    return player_choice
+choices = {"plaer":player_choice, "computer": computer_choice}
+
+
+
+choices= get_choices()
