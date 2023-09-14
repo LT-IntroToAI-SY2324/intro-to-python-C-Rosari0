83df8c5a7a20bab2f7c2cc3ea4ff041696df506c
Cristian <cnrosario@cps.edu>
2023-09-14 11:15:38 -0500

    a1.py

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -17,3 +17,12 @@ assert fizbuzz(5) == "Buzz", "fizzbuzz 5 test"
 assert fizbuzz(6) == "Fizz", "fizzbuzz 6 test"
 assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test"
 
+def fizzbuzz(input_num):
+    if(input_num/3==0):
+        if(input_num/5==0):
+            return 'FizzBuzz'
+        return'Fizz'
+    elif(input_num/5==0):
+        return 'Buzz'
+    else:
+        return input_num
